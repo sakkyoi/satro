793c25c0e4af9a7da74c7aacab872c6f0ba9e29e
sakkyoi <22865542+sakkyoi@users.noreply.github.com>
2024-09-15 17:23:16 +0800

    Refactor footer rendering logic and add fix support for script elements

--- src/content/config.ts
+++ src/content/config.ts
@@ -25,11 +25,39 @@ const announcement = defineCollection({
 const footer = defineCollection({
 	type: 'content',
 	schema: z.object({
-		type: z.enum(['link', 'script']).optional(),
-		title: z.string().optional(),
-		url: z.string().optional(),
-		target: z.enum(['_blank', '_self']).optional(),
-		icon: z.string().optional(),
+		type: z.enum(['link', 'script']),
+		name: z.string().optional(), // link
+		icon: z.string().optional(), // link (if icon is set, body will be ignored)
+		attributes: z.union([
+			z.object({
+				href: z.string(),
+				target: z.enum(['_blank', '_self']).optional(),
+			}),
+			z.object({
+				src: z.string(),
+				async: z.boolean().optional(),
+				crossorigin: z.enum(['anonymous', 'use-credentials']).optional(),
+				defer: z.boolean().optional(),
+				integrity: z.string().optional(),
+				nomodule: z.boolean().optional(),
+				referrerpolicy: z.enum(['no-referrer', 'no-referrer-when-downgrade', 'origin', 'origin-when-cross-origin', 'same-origin', 'strict-origin', 'strict-origin-when-cross-origin', 'unsafe-url']).optional(),
+				type: z.string().optional(),
+			}).optional(),
+		]),
+	}).superRefine((data, ctx) => {
+		if (data.type === 'link') {
+			if (!data.name) {
+				ctx.addIssue({
+					code: z.ZodIssueCode.custom,
+					message: 'Title is required for link type',
+				});
+			}
+		} else if (data.type === 'script' && (data.name || data.icon)) {
+			ctx.addIssue({
+				code: z.ZodIssueCode.custom,
+				message: 'Title and icon are not allowed for script type',
+			});
+		}
 	}),
 });
 
