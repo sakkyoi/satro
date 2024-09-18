d0d5fb3770aea493f8fb64a09d982fee69ea9a58
sakkyoi <22865542+sakkyoi@users.noreply.github.com>
2024-09-18 19:27:34 +0800

    fix namespace of sitemap index

--- src/pages/sitemap-index.xml.ts
+++ src/pages/sitemap-index.xml.ts
@@ -8,7 +8,7 @@ export async function GET() {
 
     let sitemap = '';
     sitemap += '<?xml version="1.0" encoding="UTF-8"?>';
-    sitemap += '<sitemapindex>';
+    sitemap += '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
     for (const sitemapName of sitemaps) {
         sitemap += `<sitemap><loc>${siteUrl}/sitemap-${sitemapName}.xml</loc></sitemap>`;
