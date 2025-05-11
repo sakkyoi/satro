b48368ae2cc5559a33d4b7f3231a3eabed47a6aa
sakkyoi <22865542+sakkyoi@users.noreply.github.com>
2025-05-11 19:11:16 +0800

    add mermaid chart support

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { expressiveCodePatcher } from './src/expressive-code-patcher';
 import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections';
 import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
 import mdx from '@astrojs/mdx';
+import rehypeMermaid from 'rehype-mermaid';
 import tailwind from '@astrojs/tailwind';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
@@ -63,6 +64,7 @@ export default defineConfig({
                 }
             ],
             scrollspyPatcher, // hey! put this after rehypeSlug and rehypeAutolinkHeadings
+            rehypeMermaid,
         ],
         remarkPlugins: [remarkMath],
     },
